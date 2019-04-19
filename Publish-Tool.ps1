5093e7299e92a9f6393ed72ae3e783204bbc747d
OpportunityLiu <opportunity@live.in>
2019-04-19 17:16:26 +0800

    fix

--- Publish-Tool.ps1
+++ Publish-Tool.ps1
@@ -4,5 +4,5 @@ $root = Split-Path -Parent $MyInvocation.MyCommand.Source
 Set-Location $root/EhDbReleaseBuilder
 Remove-Item ..\..\Database\tools\EhDbReleaseBuilder\ -Recurse -Exclude '*.md'
 dotnet publish -c release -o ..\..\Database\tools\EhDbReleaseBuilder\
-
+Remove-Item ..\..\Database\tools\EhDbReleaseBuilder\ -Recurse -Include '*.pdb'
 Set-Location $loc
